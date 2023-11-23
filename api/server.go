07f5f10423ef5cc03690c4c5687639683f140f87
Paulo Rocha <pauloyr@outlook.com>
2023-11-22 21:33:03 -0300

    Revert "pass paths"
    This reverts commit 61b3ad55f947420eb587764be10b08a81b5487b2.

--- api/server.go
+++ api/server.go
@@ -52,9 +52,7 @@ func (s *Server) Start(addr string) error {
 			"/v1/recipe/:id/comment",
 			"/v1/recipe/:id/like",
 			"/v1/recipe/:id",
-			"/v1/recipe/more-like",
-			"/v1/user/:id/recipe",
-			"/v1/user/:id/recipe/like":
+			"/v1/recipe/more-like":
 			ctx.Next()
 			return
 		}
